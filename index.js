835b37aaa90f0753190fe838034ab459326573d1
Ivan Andreev <ivanandreev.mail@gmail.com>
2017-10-16 01:21:09 +0300

    yandex mail approve

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ app.get('/', function(request, response) {
   response.render('index.html');
 });
 
+
+app.get('/b2f4745f6637.html', function(request, response) {
+  response.render('b2f4745f6637.html');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
