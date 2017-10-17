805336588af1178cf73a49d29b22d3dedcd95b3e
Ivan Andreev <ivanandreev.mail@gmail.com>
2017-10-18 00:05:53 +0300

    mailer

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 var express = require('express');
 var app = express();
+var nodemailer = require('nodemailer');
+
+
+var transporter = nodemailer.createTransport({
+  service: 'yandex',
+  auth: {
+    user: 'test@hr-mindal.ru',
+    pass: 'test12'
+  }
+});
+
+var mailOptions = {
+  from: 'request@hr-mindal.ru',
+  to: 'info@hr-mindal.ru',
+  subject: 'Test message',
+  text: 'Hello world!'
+};
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/html'));
-
-// views is directory for all template files
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'html');
 
 app.get('/', function(request, response) {
   response.render('index.html');
@@ -16,6 +29,17 @@ app.get('/', function(request, response) {
 
 app.get('/b2f4745f6637.html', function(request, response) {
   response.render('b2f4745f6637.html');
+});
+
+app.post('/mail', function(req, res) {
+  transporter.sendMail(mailOptions, function(error, info) {
+    if(error) {
+      console.log(error);
+      res.json({msg: 'error'});
+    } else {
+      res.json({msg: info.response});
+    }
+  });
 });
 
 app.listen(app.get('port'), function() {
